aeb06c16eb7173dbd0d9faf912651ee1a5a68ff8
huyhuynh1502 <huy.huynh@dal.ca>
2024-05-11 18:25:03 -0300

    [Add] variables to Single Note class component

--- components/SingleNote.jsx
+++ components/SingleNote.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+/**
+ * Single Note Component
+ * A single page note view with a title and content
+ */
+
 const SingleNote = () => {
-  return (
-    <div>SingleNote</div>
-  )
+    let heading = 'Title';
+    let content = 'Content';
+
+    return (
+        <>
+            <h1>Single Note</h1>
+        </>
+
+    )
 }
 
 export default SingleNote
